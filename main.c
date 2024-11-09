b6cf39f8c63b021f94ae6a45ff86a837804b1229
Denis Lafin <dslf@live.ru>
2024-11-10 02:23:37 +0300

    clean

--- main.c
+++ main.c
@@ -5,10 +5,6 @@ int main(int argc, char *argv[]) {
   printf("Skrambilka %s by dslf (%s %s)\n\n", P_VER, C_DATE, C_TIME);
 
   init_cube();
-  /* rotate_cube("R U R' U'");
-  draw_cube(); */
   parse_argc(argc, argv);
-  
-
   return 0;
 }
