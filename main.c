8f358aca3ca5a9cfeaccb576f77abd43880f9c35
Denis Lafin <dslf@live.ru>
2024-11-09 04:31:55 +0300

    Test clean

--- main.c
+++ main.c
@@ -1,20 +1,9 @@
-/* #include <stdio.h> */
 #include "defee39.h"
 
-/* #include "somfnc8re3.h" */
-
 int main(int argc, char *argv[]) {
-/*   if (argc < 2) {
-    printf(
-        "Usage: %s \"Scramble\"\nExample: %s \"U L' B R2 U2 F U2 B R2 U2 F' "
-        "U2 F' D' R U' B' L U' B\"\n\n",
-        argv[0], argv[0]);
-    return 0;
-  } */
 
   init_cube();
   parse_argc(argc, argv);
-  /* read_line(atoi(argv[1])); */
 
   return 0;
 }
