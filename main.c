7f145b3b28a085612b4ee5b147ae4f918bed098a
Denis Lafin <dslf@live.ru>
2024-11-10 02:22:49 +0300

    add header

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include "defee39.h"
 
 int main(int argc, char *argv[]) {
+  printf("Skrambilka %s by dslf (%s %s)\n\n", P_VER, C_DATE, C_TIME);
 
   init_cube();
   /* rotate_cube("R U R' U'");
